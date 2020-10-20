238ab794e6e6b5bf8922f94246684c31c9f3b474
Sam Stratter <humanharddrive@gmail.com>
2020-10-19 21:46:27 -0400

    Setup some basic element parsing

--- UI Examples/WX Example/WXExampleFrame.cpp
+++ UI Examples/WX Example/WXExampleFrame.cpp
@@ -1,12 +1,102 @@
 #include "WXExampleFrame.h"
 
+#include "wx/filedlg.h"
+#include "wx/button.h"
+
 #include <iostream>
+
+#include "boost/property_tree/xml_parser.hpp"
 
 WXExampleFrame::WXExampleFrame(wxWindow * parent) :
 	WXExampleFrameBase(parent)
 {
+	//Setup element parsing functions
+	m_ParseFnMap[BUTTON_ELEMENT]		= [this](boost::property_tree::ptree& pt) { return parseButton(pt); };
+	m_ParseFnMap[MENULIST_ELEMENT]		= [this](boost::property_tree::ptree& pt) { return parseMenuList(pt); };
+	m_ParseFnMap[RADIO_GROUP_ELEMENT]	= [this](boost::property_tree::ptree& pt) { return parseRadioGroup(pt); };
+
+	//Create the basic sizer needed for WX widgets
+	m_BasicSizer = new wxBoxSizer(wxVERTICAL);
+	SetSizer(m_BasicSizer);
 }
 
 void WXExampleFrame::OnOpenPlugin(wxCommandEvent & /*event*/)
 {
+	wxFileDialog dlg(this, _("Open File"), "", "", "XUL File (*.xml)|*.xml", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+
+	if (dlg.ShowModal() == wxID_CANCEL)
+		return;
+
+	//Clear out all of the old UI elements
+	while (!m_aPluginObjects.empty())
+	{
+		delete m_aPluginObjects.top();
+		m_aPluginObjects.pop();
+	}
+
+	parsePluginUI(std::string(dlg.GetPath()));
+}
+
+bool WXExampleFrame::parsePluginUI(const std::string & sFilePath)
+{
+	boost::property_tree::ptree pt;
+
+	try
+	{
+		boost::property_tree::read_xml(sFilePath, pt);
+		return parseWindow(pt);
+	}
+	catch (boost::property_tree::ptree_error& e)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool WXExampleFrame::parseWindow(boost::property_tree::ptree & pt)
+{
+	if (pt.get_child_optional("window"))
+	{
+		for (auto& v : pt.get_child("window"))
+		{
+			std::string sTag = v.first;
+			if (sTag == "<xmlattr>")
+			{
+				std::string sID = v.second.get("id", "");
+				std::string sTitle = v.second.get("title", "");
+			}
+			else
+			{
+				if (m_ParseFnMap.find(sTag) != m_ParseFnMap.end())
+					m_ParseFnMap[sTag](v.second);
+			}
+		}
+
+		Layout();
+		return true;
+	}
+
+	return false;
+}
+
+bool WXExampleFrame::parseButton(boost::property_tree::ptree & pt)
+{
+	std::string sLabel = pt.get("<xmlattr>.label", "");
+	
+	wxButton* pButton = new wxButton(this, wxID_ANY, sLabel);
+	m_BasicSizer->Add(pButton, 0, wxALL, 5);
+	m_aPluginObjects.push(pButton);
+
+	return true;
+}
+
+bool WXExampleFrame::parseMenuList(boost::property_tree::ptree & pt)
+{
+	return false;
+}
+
+bool WXExampleFrame::parseRadioGroup(boost::property_tree::ptree & pt)
+{
+	return false;
 }
--- UI Examples/WX Example/WXExampleFrame.h
+++ UI Examples/WX Example/WXExampleFrame.h
@@ -1,6 +1,22 @@
 #pragma once
 
 #include "WXExampleBase.h"
+#include "wx/sizer.h"
+
+#include <stack>
+#include <string>
+#include <map>
+#include <functional>
+
+#include "boost/property_tree/ptree.hpp"
+
+/*https://developer.mozilla.org/en-US/docs/Archive/Mozilla/XUL*/
+
+#define WINDOW_ELEMENT			"window"
+#define BUTTON_ELEMENT			"button"
+#define MENULIST_ELEMENT		"menulist"
+#define RADIO_GROUP_ELEMENT		"radiogroup"
+#define RADIO_BUTTON_ELEMENT	"radio"
 
 class WXExampleFrame : public WXExampleFrameBase
 {
@@ -9,4 +25,17 @@ public:
 
 protected:
 	virtual void OnOpenPlugin(wxCommandEvent& event);
+
+private:
+	std::stack<wxObject*> m_aPluginObjects;
+	std::map<std::string, std::function<bool(boost::property_tree::ptree&)>> m_ParseFnMap;
+
+	wxBoxSizer* m_BasicSizer;
+
+	bool parsePluginUI(const std::string& sFilePath);
+
+	bool parseWindow(boost::property_tree::ptree& pt);
+	bool parseButton(boost::property_tree::ptree& pt);
+	bool parseMenuList(boost::property_tree::ptree& pt);
+	bool parseRadioGroup(boost::property_tree::ptree& pt);
 };
